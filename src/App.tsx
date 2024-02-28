e8b4f2765d392d5dbc38b63189fb4c97691680aa
huyikai <hyk_tiger@foxmail.com>
2024-02-28 16:26:07 +0800

    change dev tool

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Timeline } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App: React.FC = () => (
+  <Timeline
+    mode="alternate"
+    items={[
+      {
+        children: 'Create a services site 2015-09-01'
+      },
+      {
+        children: 'Solve initial network problems 2015-09-01'
+      },
+      {
+        children: 'Technical testing 2015-09-01'
+      },
+      {
+        children: 'Network problems being solved 2015-09-01'
+      }
+    ]}
+  />
+);
 export default App;
